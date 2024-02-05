a10b9e7e0d46024aeef38bf2126eaf05d544771c
navlesh0603 <122613660+navlesh0603@users.noreply.github.com>
2024-02-05 14:03:17 +0530

    home page

---
+++ lib/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple,
+      body: Container(
+        // color: Color.fromARGB(255, 124, 21, 153),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.asset(
+                "assets/images/quiz-logo.png",
+                width: 300,
+                height: 400,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              "Learn Flutter the Fun way!",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text(
+                "start quiz",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz_app/home_page.dart';
 void main() {
   runApp(MyApp());
 }
@@ -9,52 +10,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.purple,
-      body: Container(
-        // color: Color.fromARGB(255, 124, 21, 153),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Image.asset(
-                "assets/images/quiz-logo.png",
-                width: 300,
-                height: 400,
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Text(
-              "Learn Flutter the Fun way!",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const ElevatedButton(
-              onPressed: null,
-              child: Text(
-                "start quiz",
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
