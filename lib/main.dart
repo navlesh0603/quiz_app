86bdbf18548ce52221daf0178032d8d3e4872ab2
navlesh0603 <122613660+navlesh0603@users.noreply.github.com>
2024-02-07 23:10:57 +0530

    rendering  content conditionally

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,7 @@
 import 'package:flutter/material.dart';
-
-import 'package:quiz_app/home_page.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const Quiz());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.purple,
-                Colors.deepPurple,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const HomePage()),
-    ));
-  }
-}
---
+++ lib/quiz.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:quiz_app/home_page.dart';
+import 'package:quiz_app/questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activescreen = const HomePage();
+
+  void switchScreen() {
+    setState(() {
+      activescreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.purple,
+                Colors.deepPurple,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activescreen,
+        ),
+      ),
+    );
+  }
+}
