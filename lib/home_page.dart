7ac579caa9cee5f4ff5307fb11d0631dabcc428f
navlesh0603 <122613660+navlesh0603@users.noreply.github.com>
2024-02-07 14:49:27 +0530

    home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,48 +1,50 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({super.key});
-  
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.purple,
-      body: Container(
-        // color: Color.fromARGB(255, 124, 21, 153),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Image.asset(
-                "assets/images/quiz-logo.png",
-                width: 300,
-                height: 400,
-              ),
+    return Center(
+      // color: Color.fromARGB(255, 124, 21, 153),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Center(
+            child: Image.asset(
+              "assets/images/quiz-logo.png",
+              width: 300,
+              color:const Color.fromARGB(180, 255, 255, 255),
+              // height: 400,
             ),
-            const SizedBox(
-              height: 30,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Text(
+            "Learn Flutter the Fun way!",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
             ),
-            const Text(
-              "Learn Flutter the Fun way!",
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: null,
+            icon: const Icon(
+              Icons.arrow_right_alt,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+            label: const Text(
+              "start quiz",
               style: TextStyle(
                 color: Colors.white,
-                fontSize: 28,
               ),
             ),
-            const SizedBox(
-              height: 30,
-            ),
-            const ElevatedButton(
-              onPressed: null,
-              child: Text(
-                "start quiz",
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 
 import 'package:quiz_app/home_page.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-    );
+        home: Scaffold(
+      body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.purple,
+                Colors.deepPurple,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const HomePage()),
+    ));
   }
 }
